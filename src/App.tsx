3e4b75281ba7e36595cf590d56198458e64faaa7
thomasmatlock <46877252+thomasmatlock@users.noreply.github.com>
2022-09-14 11:02:01 -0700

    working error free

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import "./styles.css";
+import './styles.css';
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Hello world</h1>
+			<h2>Start editing to see some magic happen!</h2>
+		</div>
+	);
 }
